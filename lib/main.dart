a2590cb16ef1f7093bddcda448fc13a2c0d6eecb
Hitesh <Hiteshkhatri910@gmail.com>
2024-04-30 15:01:42 +0530

    FestivalPostApp

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:festival_post_app/utils/app_routes.dart';
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: app_routes,
+    )
+  );
+}
---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,5 @@
+import 'package:festival_post_app/screen/home/home_screen.dart';
+import 'package:flutter/material.dart';
+Map<String,WidgetBuilder> app_routes = {
+  "/":(context) => HomeScreen()
+};
